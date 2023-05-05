f0bb0fe06df1ef56fb5a8bc45118e4ed089941dc
marius0907 <marius.ionita0907@gmail.com>
2023-05-05 18:04:36 +0300

    curs1

---
+++ src/curs1/Tester.java
@@ -0,0 +1,20 @@
+package curs1;
+
+public class Tester {
+private String nume ;
+
+    public int ratePerHour ;
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+
+    public static void main (String args[]){
+        System.out.println("higit ");
+    }
+}
